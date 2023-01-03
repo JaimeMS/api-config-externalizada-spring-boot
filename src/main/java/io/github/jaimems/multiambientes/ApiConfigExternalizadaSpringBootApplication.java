382976704722e2249caf8d0481a96d73c20aaa33
Jaime <santos.jaimemendes91@gmail.com>
2023-01-03 20:17:18 -0300

    Iniciando o projeto

---
+++ src/main/java/io/github/jaimems/multiambientes/ApiConfigExternalizadaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.github.jaimems.multiambientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConfigExternalizadaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConfigExternalizadaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jaimems/multiambientes/ApiConfigExternalizadaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jaimems.multiambientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConfigExternalizadaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
